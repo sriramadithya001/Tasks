13ce3488ab01bec3764aa025209c298bf714a3d3
sriramadithya001 <143174882+sriramadithya001@users.noreply.github.com>
2023-10-05 22:46:50 +0530

    Add files via upload

---
+++ Day3/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    public static void main(String[] args) {
+        class createEmployee {
+            String name;
+            int year_of_joining;
+            int salary;
+            String address;
+            
+            public createEmployee(String name, int year_of_joining, int salary, String address) {
+                this.name = name;
+                this.year_of_joining = year_of_joining;
+                this.salary = salary;
+                this.address = address;
+            }
+            
+            public void printEmployee() {
+                System.out.println(this.name + "\t" + this.salary + "\t" + this.year_of_joining + "\t" + this.address);
+            }
+            
+        }
+        createEmployee employee_1 = new createEmployee("john",30000, 2012, "XYZ Street");
+        createEmployee employee_2 = new createEmployee("kumar",35000, 2013, "XYY Street");
+        createEmployee employee_3 = new createEmployee("johny",33000, 2012, "YYZ Street");
+        
+        System.out.println("Name    salary      Year_of_joining     Address");
+        employee_1.printEmployee();
+        employee_2.printEmployee();
+        employee_3.printEmployee();
+    }
+}
