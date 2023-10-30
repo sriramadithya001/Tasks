a1ed3adf1651da30820658e78ef6137e1bb3fba1
sriramadithya001 <143174882+sriramadithya001@users.noreply.github.com>
2023-10-30 12:11:41 +0530

    Add files via upload

---
+++ SpecialString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SpecialString {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String s = sc.next();int n = s.length(), sp = 0;
+	for(int i=0;i<n;i++) {
+	    for(int j=i;j<n;j++) {
+		boolean[] cnt = new boolean[26];
+		for(int k=i;k<=j;k++) cnt[s.charAt(k) - 'a'] = true;
+		int count = 0;
+		for(int k=0;k<26;k++) count += (cnt[k] ? 1 : 0);
+		if(count == 1) sp++;
+		else if(count ==2 && (j-i+1)%2==1) sp++;
+	    }
+	}
+	System.out.println(sp);
+    }
+}
