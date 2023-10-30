7b10a6a3f84406b008d40d314f9e90309be12370
sriramadithya001 <143174882+sriramadithya001@users.noreply.github.com>
2023-10-30 12:44:45 +0530

    Update SpecialString.java

--- SpecialString.java
+++ SpecialString.java
@@ -6,10 +6,10 @@ public class SpecialString {
 	String s = sc.next();int n = s.length(), sp = 0;
 	for(int i=0;i<n;i++) {
 	    for(int j=i;j<n;j++) {
-		boolean[] cnt = new boolean[26];
-		for(int k=i;k<=j;k++) cnt[s.charAt(k) - 'a'] = true;
+		int[] cnt = new int[26];
+		for(int k=i;k<=j;k++) cnt[s.charAt(k) - 'a']++;
 		int count = 0;
-		for(int k=0;k<26;k++) count += (cnt[k] ? 1 : 0);
+		for(int k=0;k<26;k++) count += (cnt[k]!=0 ? 1 : 0);
 		if(count == 1) sp++;
 		else if(count ==2 && (j-i+1)%2==1 && cnt[s.charAt(j-i+1)-'a']==1) sp++;
 	    }
